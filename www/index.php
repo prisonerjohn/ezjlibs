81ecba63fd4632acf885499f5e196becc0684f86
prisonerjohn <prisonerjohn@fcf2b2f2-ae05-11dd-aaf8-13f29377832e>
2008-11-19 15:06:50 +0000

    Updated library revision number.
    git-svn-id: https://ezjlibs.googlecode.com/svn/trunk@40 fcf2b2f2-ae05-11dd-aaf8-13f29377832e

--- www/index.php
+++ www/index.php
@@ -21,8 +21,8 @@
 			
 		<p>If you're going to analyze <a href="http://en.wikipedia.org/wiki/Wii_Remote">Wiimote</a> gestures, you'll need to install Andreas Schlegel's <a href="http://www.sojamo.de/libraries/oscP5/">oscP5</a> library, and you'll need to run his <a href="http://code.google.com/p/darwiinosc/">darwiinosc</a> application.</p>
 		
-		<p><span class="big">Download the <a href="http://ezjlibs.googlecode.com/files/ezgestures-0.38.zip">library</a></span><br/>
-			version .38<br/>
+		<p><span class="big">Download the <a href="http://ezjlibs.googlecode.com/files/ezgestures-0.40.zip">library</a></span><br/>
+			version .40<br/>
 			updated on Nov 19 2008<br/>
 			unzip and drop the folder in /path/to/processing/libraries/</p>
 		
